a24dbd6fc4d969f2726a5ea955c51635bd200b93
bojidar.tonchev <bojidar.tonchev@gmail.com>
2017-06-10 19:02:19 +0300

    ~ Modified player movement script

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,63 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : PhysicsObject
-{
-    [HideInInspector]
-    public bool facingRight = true;         // For determining which way the player is currently facing.
+public class PlayerControl : MonoBehaviour {
 
-    public float maxSpeed = 7;
-    public float jumpTakeOffSpeed = 7;
-    
-    private Animator animator;
+    public float moveSpeed;
+    Animator anim;
+    public float jumpForce;
+    //float VelX;
+    //float VelY;
 
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+
+    private bool facingRight = true;
+    Rigidbody2D rigBody;
     // Use this for initialization
-    void Awake()
-    {
-        animator = GetComponent<Animator>();
+    void Start () {
+        facingRight = true;
+        rigBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    protected override void ComputeVelocity()
-    {
-        Vector2 move = Vector2.zero;
+    // Update is called once per frame
+    void FixedUpdate () {
 
-        move.x = Input.GetAxis("Horizontal");
+        grounded = Physics2D.OverlapCircle(groundCheck.position,groundRadius, whatIsGround);
+        anim.SetBool("Ground", grounded);
 
-        if (Input.GetButtonDown("Jump") && grounded)
-        {
-            velocity.y = jumpTakeOffSpeed;
+        anim.SetFloat("vSpeed", rigBody.velocity.y);
+
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+        Flip(horizontal);
+        if ( facingRight) {
+
+            anim.SetFloat("Speed", horizontal * moveSpeed);
         }
-        else if (Input.GetButtonUp("Jump"))
+        else
         {
-            if (velocity.y > 0)
-            {
-                velocity.y = velocity.y * 0.5f;
-            }
+
+            anim.SetFloat("Speed", horizontal * -moveSpeed);
+        }
+    }
+    private void Update()
+    {
+        if(grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetBool("Ground",!grounded);
+            rigBody.AddForce(new Vector2 (0, jumpForce));
+            
+        }
+    }
+
+    private void HandleMovement(float horizontal) {
+
+        rigBody.velocity = new Vector2(horizontal * moveSpeed, rigBody.velocity.y);
+    }
+    private void Flip(float horizontal) {
+        if (horizontal > 0  && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
         }
 
-        // If the input is moving the player right and the player is facing left...
-        if (move.x > 0 && !facingRight)
-            // ... flip the player.
-            Flip();
-        // Otherwise if the input is moving the player left and the player is facing right...
-        else if (move.x < 0 && facingRight)
-            // ... flip the player.
-            Flip();
-
-        //animator.SetBool("grounded", grounded);
-        animator.SetFloat("Speed", Mathf.Abs(move.x));
-
-        targetVelocity = move * maxSpeed;
     }
 
-    void Flip()
-    {
-        // Switch the way the player is labelled as facing.
-        facingRight = !facingRight;
-
-        // Multiply the player's x local scale by -1.
-        Vector3 theScale = transform.localScale;
-        theScale.x *= -1;
-        transform.localScale = theScale;
-    }
 }
